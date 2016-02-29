6539c2d1ce9315bca58070fa64cb7adaded18ecc
Dave Korhumel <dpk2442@gmail.com>
2016-02-29 12:49:07 -0500

    add simple automated TicTacToe client

---
+++ python/example_tictactoe_simple.py
@@ -0,0 +1,37 @@
+from stratum_client import StratumClient, main
+
+class TicTacToeClient(StratumClient):
+
+    def __init__(self, settings):
+        super(TicTacToeClient, self).__init__(settings)
+        self._board = None
+        self._winner = None
+
+    def server_closed_connection(self):
+        print("Game Over!")
+        if self._winner:
+            print("Player {} wins!".format(self._winner))
+        else:
+            print("Draw!")
+
+    def _find_empty_cell(self):
+        for r, row in enumerate(self._board):
+            for c, cell in enumerate(row):
+                if cell is None:
+                    return r, c
+
+    def message_received_from_server(self, message):
+        if message["type"] == "state":
+            self._board = message["board"]
+            self._winner = message["winner"]
+        elif message["type"] == "turn":
+            row, col = self._find_empty_cell()
+            self.send_message_to_server({
+                "type": "move",
+                "row": row,
+                "column": col
+            })
+
+
+if __name__ == "__main__":
+    main(TicTacToeClient)
