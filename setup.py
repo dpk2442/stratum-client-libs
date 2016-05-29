b7d1d7c1b5191a8142a571c05fab68524430b91a
Dave Korhumel <dpk2442@gmail.com>
2016-05-28 21:16:49 -0400

    change single to double quotes in setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ import sgsclient
 
 
 def readme():
-      with open('README.rst') as f:
+      with open("README.rst") as f:
             return f.read()
 
-setup(name='sgsclient',
+setup(name="sgsclient",
       version=sgsclient.version,
-      description='The python client library for the Stratum Game Server.',
+      description="The python client library for the Stratum Game Server.",
       long_description=readme(),
-      keywords=['sgsclient'],
-      url='https://github.com/stratumgs/stratumgs-python-client',
-      author='David Korhumel',
-      author_email='dpk2442@gmail.com',
-      license='MIT',
+      keywords=["sgsclient"],
+      url="https://github.com/stratumgs/stratumgs-python-client",
+      author="David Korhumel",
+      author_email="dpk2442@gmail.com",
+      license="MIT",
       packages=find_packages(),
       install_requires=[],
       include_package_data=True,
